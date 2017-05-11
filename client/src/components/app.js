044024d333c977ac284b0e19902f2de5ef193558
Laura Wolfart <laurawolfart@gmail.com>
2017-05-11 12:47:41 -0400

    nav bar draft done

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -12,7 +12,6 @@ class App extends Component {
         <div>
           <Header />
           <Home />
-          <LoginPage />
           <ImgUpload />
         </div>
     );
--- client/src/components/common/header.js
+++ client/src/components/common/header.js
@@ -4,12 +4,31 @@ const Header = () => {
   return (
     <nav>
       <div>
-        <ul>
-          <li>Home</li>
-        </ul>
-        <ul>
-          <li><button type="button">Sing In</button></li>
-        </ul>
+        <tr>
+          <td>
+            <a href="#"><h1>Let it mow</h1></a>
+          </td>
+          <td>
+            <form>
+              <div>
+                <input type="text" name="renting" placeholder="What are you renting?" />
+                <span class="search-icon"></span>
+              </div>
+              <div>
+                <input type="text" placeholder="Enter a zip code" />
+                <span class="location-icon"></span>
+              </div>
+            </form>
+          </td>
+          <td>
+          <div>
+            <a href={'/api/auth/google'}>Login with Google</a>
+          </div>
+          <div>
+            <a href="#">Rent your equipment</a>
+          </div>
+          </td>
+        </tr>
       </div>
     </nav>
   );

92430ded17b5a615c098f68a6f37d920cfe90cc3
Laura Wolfart <laurawolfart@gmail.com>
2017-05-11 11:36:19 -0400

    testing nav component

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as Cookies from 'js-cookie';
+import Header from './common/header';
 import Home from './home';
 import LoginPage from './login-page';
 import ImgUpload from './image-uploader';
@@ -10,6 +11,7 @@ class App extends React.Component {
     render() {
         return(
         <div>
+          <Header />
           <Home />
           <LoginPage />
           <ImgUpload />
---
+++ client/src/components/common/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav>
+      <div>
+        <ul>
+          <li>Home</li>
+        </ul>
+        <ul>
+          <li><button type="button">Sing In</button></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;

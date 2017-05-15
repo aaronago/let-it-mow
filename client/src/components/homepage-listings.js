b3e507211e2edb960753e4f6cfbd6568521079fb
brianmsj <brianmsj@gmail.com>
2017-05-15 15:56:47 -0700

    added search feature to homepage listings

--- client/src/components/homepage-listings.js
+++ client/src/components/homepage-listings.js
@@ -5,21 +5,38 @@ import GalleryListing from './gallery-listing';
 class HomepageListings extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchString: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
-
+  handleChange(e){
+      this.setState({searchString:e.target.value});
+    }
   render() {
-    const cards = this.props.listings.map (card => {
-      return (
-        <div key={card._id}>
-          <GalleryListing images={card.images} title={card.title} price={card.price} id={card._id}/>
-        </div>
-      );
-    });
+
+    let listings = this.props.listings
+    let searchString = this.state.searchString.trim().toLowerCase();
+    if(searchString.length > 0){
+        listings = listings.filter(function(l){
+        return l.title.toLowerCase().match(searchString);
+        });
+    }
     return (
+      <div>
+      <input type="text" value={this.state.searchString}
+      onChange={this.handleChange} placeholder="Search Equipment"/>
       <div className="listings-gallery">
-        {cards}
+      {listings.map(card => {
+        return (
+          <div key={card._id}>
+          <GalleryListing images={card.images} title={card.title}
+           price={card.price} id={card._id}/>
+       </div>)}
+     )}
       </div>
-    );
+      </div>
+   )
   }
 }
 

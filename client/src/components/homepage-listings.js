bec152a731c2109a6c686b8db75e977ce1938b38
brianmsj <brianmsj@gmail.com>
2017-05-15 20:47:38 -0700

    added dynamic filter labels to the filter

--- client/src/components/homepage-listings.js
+++ client/src/components/homepage-listings.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import GalleryListing from './gallery-listing';
 import Slider, {Range} from 'rc-slider';
+import 'rc-tooltip/assets/bootstrap.css';
 import Tooltip from 'rc-tooltip';
 import 'rc-slider/assets/index.css';
 
 function log(value) {
-  console.log(value);
+  return value
 }
 class HomepageListings extends Component {
   constructor(props) {
@@ -47,9 +48,14 @@ class HomepageListings extends Component {
         return l.title.toLowerCase().match(searchString);
         });
     }
-    if(lowPrice > 0 && highPrice < 15) {
+    if(lowPrice > 0 && highPrice < 10) {
          listings = listings.filter(function(l) {
-           return l.price < 15
+           return l.price < 10
+         })
+    }
+    if(lowPrice > 0 && highPrice < 20) {
+         listings = listings.filter(function(l) {
+           return l.price < 20
          })
     }
     if(lowPrice > 0 && highPrice < 30) {
@@ -57,15 +63,19 @@ class HomepageListings extends Component {
            return l.price < 30
          })
     }
-
-
+    if(lowPrice > 0 && highPrice < 40) {
+         listings = listings.filter(function(l) {
+           return l.price < 40
+         })
+    }
 
     return (
       <div>
       <input type="text" value={this.state.searchString}
       onChange={this.handleChange} placeholder="Search Equipment"/>
       <div className='price-filter'>
-      <h4>Filter Price</h4>
+      <h4 className='filter-title'>Filter Price</h4>
+      <p className='price-text'>{`$${lowPrice} to $${highPrice} /day`}</p>
       <Range className='range-slider'
              marks={marks}
              min={5}
@@ -73,6 +83,7 @@ class HomepageListings extends Component {
              defaultValue={[10,50]}
              allowCross={false}
              value={this.state.value}
+             tipFormatter={value => `${value} $`}
              onChange={this.onSliderChange}
        />
       </div>

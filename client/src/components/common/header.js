24d1f21ddd9b37704d3ccd4766188aa5a8f7024e
Laura Wolfart <laurawolfart@gmail.com>
2017-05-18 11:39:04 -0400

    fix errors

--- client/src/components/common/header.js
+++ client/src/components/common/header.js
@@ -4,7 +4,6 @@ import {logout} from '../../actions/index';
 import { Link } from 'react-router-dom';
 import * as Cookies from 'js-cookie';
 import img from './let-it-mow-logo.png';
-
 export class Header extends Component {
   constructor(props) {
     super(props);
@@ -41,19 +40,12 @@ export class Header extends Component {
       <div>
         <nav className="flex-nav">
           <ul>
-<<<<<<< HEAD
-            <li><Link to='/'><h1>Let it mow</h1></Link></li>
-            <li className="right">{isLoggedIn}</li>
-            <li className="right" onClick={this.authCheck}>
+            <li className="one right"><Link to='/'><img src={img} /></Link></li>
+            <li className="two right">{isLoggedIn}</li>
+            <li className="three">
               <div className={disableLink} >
                 <Link to={`/mylistings/`}>Rent your equipment</Link>
               </div>
-=======
-            <li className="one"><Link to='/'><img src={img} /></Link></li>
-            <li className="two">{isLoggedIn}</li>
-            <li className="three">
-              <Link to={`/mylistings/`}>Rent your equipment</Link>
->>>>>>> styling
             </li>
           </ul>
           <span className={hidden}>You must login to manage your rentals</span>

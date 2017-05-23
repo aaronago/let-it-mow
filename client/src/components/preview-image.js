0d78e91e7e6b6ba61625fa776831ba2aeeee8a39
Dave Hoel <dkhoel@gmail.com>
2017-05-23 10:44:49 -0700

    removes preview text JSX from preview-image as this is now being handled in preview-text

--- client/src/components/preview-image.js
+++ client/src/components/preview-image.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
- const PreviewImage = (props) => {
+const PreviewImage = (props) => {
   return (
     <div>
-        <img src={`http://res.cloudinary.com/letitmow/image/upload/w_150,h_100/${props.id}.jpg`}/>
+      <img src={`http://res.cloudinary.com/letitmow/image/upload/w_150,h_100/${props.id}.jpg`}/>
     </div>
   );
 };

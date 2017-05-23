51aff656175250e790f098fb068cb8e26f095018
brianmsj <brianmsj@gmail.com>
2017-05-22 17:48:05 -0700

    added static google map to single listing page

--- client/src/components/single-listing-page.js
+++ client/src/components/single-listing-page.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchListing, startConversation } from '../actions';
 import MoreFromSeller from './more-from-seller';
+import GoogleMapReact from 'google-map-react';
 
 
 
@@ -40,18 +41,26 @@ class SingleListingPage extends Component {
     if(!listing) {
       return <div>...Loading</div>;
     }
+    console.log(listing)
+    const position = listing.geometry.coordinates
     return (
       <div className='row single-item-listings-container'>
         <div className='col-6'>
           <img className='single-item-image' src={`http://res.cloudinary.com/letitmow/image/upload/w_500,h_700,c_scale/${listing.images[0]}.jpg`}/>
+          <h4 className='all-items-seller-heading single-item-des'>DESCRIPTION</h4>
+          <div className='static-map'>
+          <GoogleMapReact
+            defaultCenter={{lat: position[1], lng: position[0]}}
+            defaultZoom={13}>
+          </GoogleMapReact>
+          </div>
         </div>
       <div className='col-6 user-listings-container'>
         <h1>${listing.price} / Day</h1>
         <h2 className='single-listing-title'>{listing.title}</h2>
           <form action="" onSubmit={this.onSubmit}>
-            <label htmlFor="Start Chat">Start Chat</label>
-            <input type="text" onChange={this.onInputChange}/>
-            <input className='btn-square chat-with-button'type="submit" value={`Chat with ${listing.createdBy.name}`}/>
+            <input className='single-page-chat'type="text" placeholder='Type Message...' autoFocus='true' onChange={this.onInputChange}/>
+            <input className='btn-square chat-with-button'type="submit" value={`Send Chat To ${listing.createdBy.name}`}/>
           </form>
           <MoreFromSeller className='more-seller' userId={listing.createdBy} picId={this.props.match.params}/>
         </div>

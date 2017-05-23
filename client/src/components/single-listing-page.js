c8200d9c47afca09982b03e31d16b1e760325d47
brianmsj <brianmsj@gmail.com>
2017-05-22 20:20:55 -0700

    added share buttons

--- client/src/components/single-listing-page.js
+++ client/src/components/single-listing-page.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 import { fetchListing, startConversation } from '../actions';
 import MoreFromSeller from './more-from-seller';
 import GoogleMapReact from 'google-map-react';
-
-
+import FontAwesome from 'react-fontawesome'
 
 class SingleListingPage extends Component {
   constructor(props){
@@ -41,19 +40,29 @@ class SingleListingPage extends Component {
     if(!listing) {
       return <div>...Loading</div>;
     }
-    console.log(listing)
+
     const position = listing.geometry.coordinates
+
+
     return (
       <div className='row single-item-listings-container'>
         <div className='col-6'>
           <img className='single-item-image' src={`http://res.cloudinary.com/letitmow/image/upload/w_500,h_700,c_scale/${listing.images[0]}.jpg`}/>
           <h4 className='all-items-seller-heading single-item-des'>DESCRIPTION</h4>
+          <p className='single-des-p'>{listing.description}</p>
           <div className='static-map'>
           <GoogleMapReact
             defaultCenter={{lat: position[1], lng: position[0]}}
             defaultZoom={13}>
           </GoogleMapReact>
           </div>
+        <h4 className='all-items-seller-heading single-item-des'>SHARE THIS PRODUCT</h4>
+        <a href='https://github.com/Jean-Luc19/let-it-mow' className='twitter-logo faicon'>
+          <FontAwesome className='fa fa-twitter-square' size='3x' aria-hidden='true'/></a>
+        <a href='https://github.com/Jean-Luc19/let-it-mow' className='facebook-logo faicon'>
+          <FontAwesome className='fa fa-facebook-square' size='3x' aria-hidden='true'/></a>
+        <a href='https://github.com/Jean-Luc19/let-it-mow' className='email-logo faicon'>
+          <FontAwesome className='fa fa-envelope-square' size='3x' aria-hidden='true'/></a>
         </div>
       <div className='col-6 user-listings-container'>
         <h1>${listing.price} / Day</h1>

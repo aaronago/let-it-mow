2ae4540a20035859eb311959e03fc0feeaf237e9
brianmsj <brianmsj@gmail.com>
2017-05-23 22:12:57 -0700

    added more styling to dashboard

--- client/src/components/user-listings.js
+++ client/src/components/user-listings.js
@@ -81,13 +81,14 @@ class UserListings extends Component {
           onClick={this.onClick}>{formBtn}</button>
         <div className='row dashboard-listings'>
         <div className='col-7'>
-        <h2>Your Listings</h2>
+        <h2 className='dash-stat dash-title'>Your Listings</h2>
           <div className='listings-gallery'>
             {listItem}
           </div>
          </div>
          <div className='col-5 dashboard-stats'>
            <h2 className='dash-stat dash-title'>Your Info!</h2>
+           <div className='stats-container'>
            <h4 className='all-items-seller-heading dash-head'>NUMBER OF ITEMS LISTED</h4>
              <p className='dash-stat'>{listings.length}</p>
            <h4 className='all-items-seller-heading dash-head'>NEWEST LISTING</h4>
@@ -96,6 +97,7 @@ class UserListings extends Component {
              <p className='dash-stat'>{this.oldDate(listings)}</p>
            <h4 className='all-items-seller-heading dash-head'>OPEN CHATS</h4>
              <p className='dash-stat'>{this.props.conversations.length}</p>
+            </div>
          </div>
          </div>
       </div>

8db7b49be30fc7313269a2a1fd27d393202dc9f2
Aaron Gottlieb <aticus19@gmail.com>
2017-05-25 14:46:50 -0600

    adds a red background to convo box's with unread messages

--- client/src/components/listing-header-card.js
+++ client/src/components/listing-header-card.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup' ;
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { startConversation } from '../actions';
